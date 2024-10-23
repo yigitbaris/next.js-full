2032b6a1f8ae333b724e57dea8d192359c254d4c
yigitbaris <bariskaynakoglu01@gmail.com>
2024-10-24 00:43:39 +0300

    get user posts api endpoint and my profile page added

--- components/PromptCard.jsx
+++ components/PromptCard.jsx
@@ -7,6 +7,12 @@ import { usePathname, useRouter } from "next/navigation"
 const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
   const [copied, setCopied] = useState("")
 
+  const handleCopy = () => {
+    setCopied(post.prompt)
+    navigator.clipboard.writeText(post.prompt)
+    setTimeout(() => setCopied(""), 5000)
+  }
+
   return (
     <div className="prompt_card">
       <div className="flex justify-between items-start gap-5">
@@ -27,7 +33,7 @@ const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
             </p>
           </div>
         </div>
-        <div className="copy_btn" onClick={() => {}}>
+        <div className="copy_btn" onClick={handleCopy}>
           <Image
             src={
               copied === post.prompt
